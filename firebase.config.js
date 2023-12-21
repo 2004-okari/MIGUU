f6b271381e0b4961471a5732ed785f3f210833e5
okari <nyandika15@gmail.com>
2023-12-21 10:02:07 +0300

    Add Firebase config

---
+++ firebase.config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjfyKmLfMi-eWxZoQ5pZwpST7mKmnB6_c",
+  authDomain: "miguu001-b4e63.firebaseapp.com",
+  projectId: "miguu001-b4e63",
+  storageBucket: "miguu001-b4e63.appspot.com",
+  messagingSenderId: "438026763100",
+  appId: "1:438026763100:web:fd37adbb0a13bdfb45df99"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
